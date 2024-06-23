99e3cd8e4fc6313a2999ab7594ec3597448cb445
PJS <b_rok9999@mail.ru>
2024-06-23 20:41:26 +0500

    Update hw4

--- hw4.py
+++ hw4.py
@@ -40,10 +40,17 @@
 # На выходе:
 # 19
 
+# arr = [5, 8, 6, 4, 9, 2, 7, 3]
+# i = 0
+# sum = 0
+# for i in range(len(arr) - 1):
+#     if (arr[i-1] + arr[i] + arr[i+1]) > sum:
+#         sum = arr[i-1] + arr[i] + arr[i+1]
+# print(sum)
+
 arr = [5, 8, 6, 4, 9, 2, 7, 3]
-i = 0
-sum = 0
-for i in range(len(arr) - 1):
-    if (arr[i-1] + arr[i] + arr[i+1]) > sum:
-        sum = arr[i-1] + arr[i] + arr[i+1]
-print(sum)
+a = len(arr)
+b = []
+for i in range(a):
+    b.append(arr[i - 2] + arr[i - 1] + arr[i])
+print(max(b))
