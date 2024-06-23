9117b76a2f31ec6ffa153e8dbf3b4205d5ea6993
PJS <b_rok9999@mail.ru>
2024-06-23 20:56:37 +0500

    Add sem5 рекурсия

---
+++ sem5.py
@@ -0,0 +1,36 @@
+# Задача
+# Факториал числа n через рекурсию
+
+# def fac(n):
+#     if n in [0, 1]:
+#         return 1
+#     return n * fac(n - 1)
+
+# list_1 = []
+# for i in range(1, 7):
+#     list_1.append(fac(i))
+# print(list_1)
+
+# Задача №31. Общее обсуждение
+# Последовательностью Фибоначчи называется
+# последовательность чисел a0
+# , a1
+# , ..., an
+# , ..., где
+# a0
+#  = 0, a1
+#  = 1, ak
+#  = ak-1 + ak-2 (k > 1).
+# Требуется найти N-е число Фибоначчи
+# Input: 7
+# Output: 21
+
+# def fib(n):
+#     if n in [1, 2]:
+#         return 1
+#     return fib(n - 1) + fib(n - 2)
+
+# list_1 = []
+# for i in range(1, 10):
+#     list_1.append(fib(i))
+# print(list_1)
